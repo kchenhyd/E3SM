e0b4d14d9154e698e45b688dba8d0bc86810682f
Sulman, Benjamin N <sulmanbn@ornl.gov>
2020-11-30 10:46:37 -0500

    Set up initial Alquimia external model interface and EMI/stub demo simulation including C and N pools

---
+++ components/clm/src/external_models/emi/src/constants/EMI_CNNitrogenStateType_Constants.F90
@@ -0,0 +1,10 @@
+module EMI_CNNitrogenStateType_Constants
+  !
+  implicit none
+  private
+  !
+  integer, parameter, public :: L2E_STATE_NITROGEN_POOLS_VERTICALLY_RESOLVED  = 2101
+
+  integer, parameter, public :: E2L_STATE_NITROGEN_POOLS_VERTICALLY_RESOLVED  = 2102
+
+end module EMI_CNNitrogenStateType_Constants
