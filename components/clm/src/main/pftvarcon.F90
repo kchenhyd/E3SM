d42f304ad43449c00c2e4fa574465839419464d3
LaFond-Hudson S L <sqh@or-slurm-login07.ornl.gov>
2021-03-29 14:44:49 -0400

    added sal_threshold and KM_salinity to pftvarcon

--- components/clm/src/main/pftvarcon.F90
+++ components/clm/src/main/pftvarcon.F90
@@ -94,6 +94,8 @@ module pftvarcon
   real(r8), allocatable :: livewdcp(:)    !live wood (phloem and ray parenchyma) C:P (gC/gP)
   real(r8), allocatable :: deadwdcp(:)    !dead wood (xylem and heartwood) C:P (gC/gP)
 
+
+
   ! for crop
 
   ! These arrays give information about the merge of unused crop types to the types CLM
@@ -279,6 +281,9 @@ module pftvarcon
   real(r8),allocatable  :: tide_coeff_amp(:)            ! Amplitude of tide component (mm)
   real(r8),allocatable  :: tide_coeff_period(:)        ! Period of tide component (s)
   real(r8),allocatable  :: tide_coeff_phase(:)         ! Phase shift of tide component (s)
+ ! parameters for salinity response function
+  real(r8), allocatable :: sal_threshold(:) !threshold for salinity effects (ppt)
+  real(r8), allocatable :: KM_salinity(:)          !half saturation constant for omotic inhibition function (ppt)
 !endif
   !phenology
   real(r8)              :: phen_a
@@ -567,6 +572,10 @@ contains
     tide_coeff_amp(:)    = 0.0
     tide_coeff_phase(:)  = 0.0
     tide_coeff_period(:) = 1.0 ! Making period 0 would cause divide by 0 error in sinusoid calculation
+    
+    ! salinity parameters -should this be an "if defined MARSH"? -SLL
+    allocate( sal_threshold (0:mxpft) )
+    allocate( KM_salinity (0:mxpft) )
 
     ! Set specific vegetation type values
 
@@ -941,6 +950,11 @@ contains
       if (.not. readv) call endrun(msg="Error: Must specify amp, period, and phase for each tide component: i = "//trim(tempname))
       call ncd_io('tide_coeff_phase_'//trim(tempname),tide_coeff_phase(i), 'read', ncid, readvar=readv, posNOTonfile=.true.)
       if (.not. readv) call endrun(msg="Error: Must specify amp, period, and phase for each tide component: i = "//trim(tempname))
+      ! salinity parameters
+      call ncd_io('sal_threshold','sal_threshold(0:npft-1), 'read', ncid, readvar=readv, posNOTonfile=.true.)
+      if ( .not. readv ) sal_threshold(:) = 1.8_r8 !placeholder value for now-update with more accurate -SLL
+      call ncd_io('KM_salinity', 'KM_salinity(0:npft-1), 'read', ncid, readvar=readv, posNOTonfile=.true.)
+      if ( .not. readv ) KM_salinity(:) = 1.8_r8 !placeholder value for now-update with more accurate -SLL
    enddo
    if(num_tide_comps == 0) then
       write(iulog,*) "No tidal coefficients found in parameter file. Using Teri's 2-component fit values for GCREW site as default"
