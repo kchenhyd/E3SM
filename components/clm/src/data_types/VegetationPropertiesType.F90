4fdce0da990ea0b768099b6764f1a8c3aed7b446
LaFond-Hudson S L <sqh@or-slurm-login03.ornl.gov>
2021-03-30 09:52:10 -0400

    Added sal_threshold and KM_salinity to VegPropType

--- components/clm/src/data_types/VegetationPropertiesType.F90
+++ components/clm/src/data_types/VegetationPropertiesType.F90
@@ -146,6 +146,9 @@ module VegetationPropertiesType
      real(r8), allocatable :: crit_gdd2(:)        !Deciduous pheonlogy critical GDD slope
      real(r8)              :: tc_stress           !Critial temperature for moisture stress
 
+     !salinity response parameters
+     real(r8), allocatable :: sal_threshold       !Threshold for salinity effects (ppt)
+     real(r8), allocatable :: KM_salinity         !Half saturation constant for omotic inhibition function (ppt)
 
    contains
    procedure, public :: Init => veg_vp_init
@@ -183,6 +186,7 @@ contains
     use pftvarcon , only : fnr, act25, kcha, koha, cpha, vcmaxha, jmaxha, tpuha
     use pftvarcon , only : lmrha, vcmaxhd, jmaxhd, tpuhd, lmrse, qe, theta_cj
     use pftvarcon , only : bbbopt, mbbopt, nstor, br_xr, tc_stress, lmrhd, crit_gdd1, crit_gdd2
+    use pftvarcon , only : sal_threshold, KM_salinity
     !
     
     class (vegetation_properties_type) :: this
@@ -294,6 +298,9 @@ contains
     allocate( this%br_xr(0:numpft))                              ; this%br_xr(:)                 =nan
     allocate( this%crit_gdd1(0:numpft))                          ; this%crit_gdd1(:)             =nan
     allocate( this%crit_gdd2(0:numpft))                          ; this%crit_gdd2(:)             =nan
+    
+    allocate( this%sal_threshold(0:numpft))        ; this%sal_threshold(:)       =nan
+    allocate( this%KM_salinity(0:numpft))          ; this%KM_salinity(:)         =nan
     do m = 0,numpft
 
        if (m <= ntree) then
@@ -400,6 +407,8 @@ contains
         this%vmax_nfix(m)      = vmax_nfix(m)
         this%km_nfix(m)        = km_nfix(m)
         this%vmax_ptase(m)     = vmax_ptase(m)
+        this%sal_threshold(m)  = sal_threshold(m)
+        this%KM_salinity(m)    = KM_salinity(m)
 
         do j = 1 , nlevdecomp
            this%decompmicc_patch_vr(m,j) = decompmicc_patch_vr(j,m)
