3075771528efcba7857451f28ea979fa89a5c939
Sophie LaFond-Hudson <lafondhudssl@ornl.gov>
2021-04-15 14:33:03 -0400

    fixed error with qflx_tide in ColumnDataType

--- components/clm/src/biogeophys/PhotosynthesisMod.F90
+++ components/clm/src/biogeophys/PhotosynthesisMod.F90
@@ -416,6 +416,7 @@ contains
          h2o_moss_wc   => veg_ws%h2o_moss_wc                  , & !Input: [real(r8) (:)   ]  Total Moss water content
          h2osfc        => col_ws%h2osfc                       , & !Input: [real(r8) (:)   ]  Surface water
          salinity      => col_ws%salinity                        & ! Input:  [real(r8) (:)   ]  salinity (SLL 4/9/2021)
+         sal_tide      => SoilHydrologyMod%sal_tide
          )
       
       if (phase == 'sun') then
