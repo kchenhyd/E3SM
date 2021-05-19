e7f076294dc0682e74f1c36aa60ce22d08ee496b
Sophie LaFond-Hudson <lafondhudssl@ornl.gov>
2021-05-19 16:56:33 -0400

    commented out osm_inhib function in PhotosynthesisMod and added a osm_inhib function to SoilStressMod that controls effective root fraction

--- components/clm/src/biogeophys/SoilMoistStressMod.F90
+++ components/clm/src/biogeophys/SoilMoistStressMod.F90
@@ -377,7 +377,9 @@ contains
          h2osoi_vol    => col_ws%h2osoi_vol    , & ! Input:  [real(r8) (:,:) ]  volumetric soil water (0<=h2osoi_vol<=watsat) [m3/m3]
          h2osoi_liqvol => col_ws%h2osoi_liqvol , & ! Output: [real(r8) (:,:) ]  liquid volumetric moisture, will be used for BeTR
          
-         sal_threshold => veg_vp%sal_threshold              , & ! Input: [real(r8) (:)   ] Threshold for salinity effects (ppt)
+         salinity      => col_ws%salinity                      , & !Input: [real(r8) (:)   ] Salinity concentration ppt
+         osm_inhib     => veg_vp%osm_inhib                     , & !Input: [real(r8) (:)   ] osmotic inhibition factor         
+         sal_threshold => veg_vp%sal_threshold                 , & ! Input: [real(r8) (:)   ] Threshold for salinity effects (ppt)
          KM_salinity   => veg_vp%KM_salinity                  & ! Input: [real(r8) (:)   ] Half saturation constant for osmotic inhibition
          ) 
 
@@ -398,7 +400,15 @@ contains
                call soil_water_retention_curve%soil_suction(sucsat(c,j), s_node, bsw(c,j), smp_node)
                smp_node = max(smpsc(veg_pp%itype(p)), smp_node)
 
-               rresis(p,j) = min( (eff_porosity(c,j)/watsat(c,j))* &
+               !using osm_inhib to change root uptake -SLL
+               if salinity(1) .ge. sal_threshold then
+                  osm_inhib(veg_pp%itype(p)) = (1-salinity(c)/(KM_salinity(veg_pp%itype(p))+salinity(c)))
+                  rresis(p,j) = min( (eff_porosity(c,j)/watsat(c,j))* &
+                    (smp_node - smpsc(veg_pp%itype(p))) / (smpso(veg_pp%itype(p)) - smpsc(veg_pp%itype(p))), 1._r8)
+                  rresis(p,j) = rresis(p,j)*osm_inhib(veg_pp%itype(p))
+                  
+               else if salinity(1) .lt. sal_threhold then
+                  rresis(p,j) = min( (eff_porosity(c,j)/watsat(c,j))* &
                     (smp_node - smpsc(veg_pp%itype(p))) / (smpso(veg_pp%itype(p)) - smpsc(veg_pp%itype(p))), 1._r8)
 
 
